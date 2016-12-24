7d69feda9c8b31f12a56d801319e0b18751a811c
ityike <yuanfeng634@gmail.com>
2016-12-24 22:02:52 +0800

    添加子控制器

--- Weibo/AppDelegate.swift
+++ Weibo/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        sleep(2)
-        
+         
         window = UIWindow()
         window?.backgroundColor = UIColor.white
         window?.rootViewController = WeiBoMainViewController()
--- Weibo/Classes/View/Main/WeiBoMainViewController.swift
+++ Weibo/Classes/View/Main/WeiBoMainViewController.swift
@@ -12,24 +12,55 @@ class WeiBoMainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupChildControllers()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+}
+// extension 类似于oc中分类，在swift中还可以用来切分代码块
+// 可以将相近功能的函数，放在一个extension 
+// 设置界面：
+extension WeiBoMainViewController {
+    
+    // 设置所有子控制器
+    func setupChildControllers() {
+        let array = [
+            ["className": "WeiBoHomeViewController", "title": "首页", "imageName": "home"],
+            ["className": "WeiBoMessageViewController", "title": "消息", "imageName": "message"],
+            ["className": "WeiBoDiscoverViewController", "title": "发现", "imageName": "discover"],
+            ["className": "WeiBoProfileViewController", "title": "我", "imageName": "profile"]
+        ]
+        
+        var arrayWeibo = [UIViewController]()
+        for dict in array {
+            arrayWeibo.append(createController(dict: dict))
+        }
+        viewControllers = arrayWeibo
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // 使用字典创建一个子控制器
+    // [className, title, imageName]
+    private func createController(dict: [String: String]) -> UIViewController {
+        // 1 取得字典内容
+        guard let className = dict["className"],
+            let title = dict["title"],
+            let imageName = dict["imageName"],
+            let classView = NSClassFromString(Bundle.main.nameSpace + "." + className) as? UIViewController.Type else {
+            return UIViewController()
+        }
+        // 2 创建视图控制器
+        // 将className转换成class
+        //let viewController = NSClassFromString(Bundle.main.nameSpace + "." + className) as? UIViewController.Type
+        let viewController = classView.init()
+        viewController.title = title
+        
+        // 3 设置图象
+        viewController.tabBarItem.image = UIImage(named: "tabbar_" + imageName)
+        viewController.tabBarItem.selectedImage = UIImage(named: "tabbar_" + imageName + "_selected")?.withRenderingMode(.alwaysOriginal)
+        
+        let navigation = WeiBoNavigationController(rootViewController: viewController)
+        return navigation
     }
-    */
-
 }
