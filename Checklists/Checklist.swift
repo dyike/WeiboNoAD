4ba3f8b3ea3cf3b07ddc1432b6af717bbc8dd20d
ityike <yuanfeng634@gmail.com>
2016-12-30 23:14:40 +0800

    Revert "init project"
    This reverts commit a987ee62ae4e14ba1153b0ab6bf7c9bd79b88ac9.

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,49 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by yuanfeng on 2/26/16.
+//  Copyright © 2016 袁 峰. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject, NSCoding {
+    var name: String
+    var items = [ChecklistItem]()
+    var iconName: String
+
+   
+    convenience init(name: String) {
+        self.init(name: name, iconName: "No Icon")
+    }
+    
+    init(name: String, iconName: String) {
+        self.name = name
+        self.iconName = iconName
+        super.init()
+    }
+
+
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("Name") as! String
+        items = aDecoder.decodeObjectForKey("Items") as! [ChecklistItem]
+        iconName = aDecoder.decodeObjectForKey("IconName") as! String
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: "name")
+        aCoder.encodeObject(items, forKey: "Items")
+        aCoder.encodeObject(iconName, forKey: "IconName")
+    }
+    
+    //展示未完成的条数
+    func countUncheckedItems() -> Int {
+        var count = 0
+        for item in items where !item.checked {
+            count += 1
+        }
+        return count
+    }
+}
