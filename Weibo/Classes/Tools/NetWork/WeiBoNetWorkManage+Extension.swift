d79549a9c81ed05ceb27fd7b71fe810aa789f870
ityike <yuanfeng634@gmail.com>
2017-01-01 18:11:20 +0800

    下拉刷新微博数据

--- Weibo/Classes/Tools/NetWork/WeiBoNetWorkManage+Extension.swift
+++ Weibo/Classes/Tools/NetWork/WeiBoNetWorkManage+Extension.swift
@@ -10,12 +10,16 @@ import Foundation
 
 // MARK - 封装微博网络请求方法
 extension WeiBoNetWorkManager {
+    // since_id: 返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+    // max_id: 返回ID小于或等于max_id的微博，默认为0。
     // completion: 完成回调[list: 微博字典数组，isSuccess: 是否成功]
-    func statusList(completion: @escaping (_ list: [[String: AnyObject]]?, _ isSuccess: Bool) -> ()) {
+    func statusList(since_id: Int64 = 0, max_id: Int64 = 0, completion: @escaping (_ list: [[String: AnyObject]]?, _ isSuccess: Bool) -> ()) {
         let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
-//        let params = ["access_token": "2.00BVldmBGjEGPB41db88b702etzBAE"]
+        // Swift中Int可以转换成AnyObject，但是Int64不行
+        let params = ["since_id": "\(since_id)",
+                    "max_id": "\(max_id)"]
         
-        tokenRequest(URLString: urlString, parameters: nil) { (json, isSuccess) in
+        tokenRequest(URLString: urlString, parameters: params as [String : AnyObject]?) { (json, isSuccess) in
             // 从json中获取status字典数组
             let weiboInfo = json?["statuses"] as? [[String: AnyObject]]
             completion(weiboInfo, isSuccess)
--- Weibo/Classes/Tools/NetWork/WeiboStatusListModel.swift
+++ Weibo/Classes/Tools/NetWork/WeiboStatusListModel.swift
@@ -16,14 +16,21 @@ class WeiboStatusListModel {
     lazy var statusList = [WeiBoStatus]()
     
     func loadStatus(completion: @escaping (_ isSuccess: Bool) -> ()) {
-        WeiBoNetWorkManager.shared.statusList { (list, isSuccess) in
+        
+        // since_id 下拉 去除数组中第一条微博
+        let since_id = statusList.first?.id ?? 0
+        
+        
+        
+        WeiBoNetWorkManager.shared.statusList(since_id: since_id, max_id: 0) { (list, isSuccess) in
             // 字典装模型
             guard let array = NSArray.yy_modelArray(with: WeiBoStatus.self, json: list ?? []) as? [WeiBoStatus] else {
                 completion(isSuccess)
                 return
             }
             // 拼接数据
-            self.statusList += array
+            // 下拉刷新，应该将结果数组拼接在数组前面
+            self.statusList = array + self.statusList
             
             // 完成回调
             completion(isSuccess)
