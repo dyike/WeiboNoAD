542bebf1b749cd3da1f4f7c6b5ed9e6e03e4a979
ityike <yuanfeng634@gmail.com>
2017-01-03 17:58:32 +0800

    获取授权码

--- Weibo/Classes/View/Main/OAuth/WeiBoOAuthViewController.swift
+++ Weibo/Classes/View/Main/OAuth/WeiBoOAuthViewController.swift
@@ -15,19 +15,65 @@ class WeiBoOAuthViewController: UIViewController {
     override func loadView() {
         view = webView
         view.backgroundColor = UIColor.white
-        
+        // 设置代理
+        webView.delegate = self
         // 设置导航栏
         title = "登录渣浪微博"
         // 导航栏按钮
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", fontSize: 16, target: self, action: #selector(close), isBack: true)
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动登录", fontSize: 16, target: self, action: #selector(autoFill), isBack: false)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 加载授权页面
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WeiBoAppKey)&redirect_uri=\(WeiBoRedirectUri)"
+        // 确定要访问的资源
+        guard let url = URL(string: urlString) else {
+            return
+        }
+
+        // 建立请求
+        let request = URLRequest(url: url)
+        // 加载请求
+        webView.loadRequest(request)
+        
     }
     
     @objc func close() {
         dismiss(animated: true, completion: nil)
     }
+    
+//    @objc func autoFill() {
+//        // 准备js
+//        let js = "document.getElementById('userId').value = '********';" + "document.getElementById('passwd').value = '********‘;"
+//        // 让webview执行js
+//        webView.stringByEvaluatingJavaScript(from: js)
+//    }
 
+}
+
+
+extension WeiBoOAuthViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        // 如果请求地址包含 https://www.baidu.com/ 不加载页面，否则加载页面
+        // request.url?.absoluteString.hasPrefix(WeiBoRedirectUri) 返回值是true/false/nil
+        if request.url?.absoluteString.hasPrefix(WeiBoRedirectUri) == false {
+            return true
+        }
+        
+//        print("加载请求----\(request.url?.absoluteString)")
+//        print("加载请求-----\(request.url?.query)")
+        // 从 https://www.baidu.com/ 回调地址中，查找字符串code，如果有，就授权成功
+        if request.url?.query?.hasPrefix("code=") == false {
+            print("取消授权")
+            return false
+        }
+        // 从query中取出授权码
+        let code = request.url?.query?.substring(from: "code=".endIndex)
+        print("获取授权码\(code)")
+        
+        return false
+    }
 }
