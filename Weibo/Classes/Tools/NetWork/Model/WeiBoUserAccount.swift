a28461c86aa5744938c666eadec52d4e311065e2
ityike <yuanfeng634@gmail.com>
2017-01-05 21:37:58 +0800

    token过期日期的处理

--- Weibo/Classes/Tools/NetWork/Model/WeiBoUserAccount.swift
+++ Weibo/Classes/Tools/NetWork/Model/WeiBoUserAccount.swift
@@ -15,7 +15,13 @@ class WeiBoUserAccount: NSObject {
     // 用户id
     var uid: String?
     // access_token 的生命周期
-    var expires_in: TimeInterval = 0.0
+    var expires_in: TimeInterval = 0.0 {
+        didSet {
+            expiresDate = Date(timeIntervalSinceNow: expires_in)
+        }
+    }
+    // 过期日期
+    var expiresDate: Date?
     
     override var description: String {
         return yy_modelDescription()
