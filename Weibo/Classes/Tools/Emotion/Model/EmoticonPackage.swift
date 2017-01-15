8f06bd7374eb7ad3e2651cc978c34a3877afe7bd
ityike <yuanfeng634@gmail.com>
2017-01-15 16:51:29 +0800

    在 directory 的 didSet 加载表情模型数组

--- Weibo/Classes/Tools/Emotion/Model/EmoticonPackage.swift
+++ Weibo/Classes/Tools/Emotion/Model/EmoticonPackage.swift
@@ -15,7 +15,23 @@ class EmoticonPackage: NSObject {
     // 表情包分组名
     var groupName: String?
     // 表情包目录，从目录下加载info.plist可以创建表情模型数组
-    var directory: String?
+    var directory: String? {
+        didSet {
+            // 当前设置目录树，从目录下加载info.plist
+            guard let directory = directory,
+                let path = Bundle.main.path(forResource: "HMEmoticon.bundle", ofType: nil),
+                let bundle = Bundle(path: path),
+                let infoPath = bundle.path(forResource: "info.plist", ofType: nil, inDirectory: directory),
+                let array = NSArray(contentsOfFile: infoPath) as? [[String: String]],
+                let models = NSArray.yy_modelArray(with: Emoticon.self, json: array) as? [Emoticon] else {
+                    return
+            }
+            
+            // 设置表情模型数组
+            emoticons += models
+            
+        }
+    }
     
     // 懒加载表情模型的空数组
     // 使用懒加载可以避免后续的解包
