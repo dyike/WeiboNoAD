081e1e6d252430b5963f51f3563b54c20ef8a476
ityike <yuanfeng634@gmail.com>
2017-01-22 19:47:10 +0800

    图片动画

--- Weibo/Classes/View/Home/WeiBoPhoto/WeiBoPhotoBrowserViewCell.swift
+++ Weibo/Classes/View/Home/WeiBoPhoto/WeiBoPhotoBrowserViewCell.swift
@@ -12,34 +12,16 @@ import SDWebImage
 class WeiBoPhotoBrowserViewCell: UICollectionViewCell {
     var picURL: String? {
         didSet {
-            guard let picURL = picURL,
-                let image = SDWebImageManager.shared().imageCache.imageFromDiskCache(forKey: picURL) else {
-                return
-            }
-            
-            
-            // 计算imageView的frame
-            let x: CGFloat = 0
-            let width = UIScreen.main.bounds.width
-            let height = (width / image.size.width) * image.size.height
-            
-            var y: CGFloat = 0
-            if height > UIScreen.main.bounds.height {
-                y = 0
-            } else {
-                y = (UIScreen.main.bounds.height - height) * 0.5
-            }
-            imageView.frame = CGRect(x: x, y: y, width: width, height: height)
-            
-            // 设置图片
-            imageView.image = image
-            
+            setupContent(picURL: picURL)
         }
     }
+    
     
     lazy var scrollView: UIScrollView = UIScrollView()
     
     lazy var imageView: UIImageView = UIImageView()
+    
+    lazy var progressView: WeiBoProgressView = WeiBoProgressView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -54,9 +36,59 @@ class WeiBoPhotoBrowserViewCell: UICollectionViewCell {
 extension WeiBoPhotoBrowserViewCell {
     func setupUI() {
         contentView.addSubview(scrollView)
+        contentView.addSubview(progressView)
         scrollView.addSubview(imageView)
         
         scrollView.frame = contentView.bounds
+        progressView.bounds = CGRect(x: 0, y: 0, width: 50, height: 50)
+        progressView.center = CGPoint(x: UIScreen.main.bounds.width * 0.5, y: UIScreen.main.bounds.height * 0.5)
+        
+        progressView.isHidden = true
+        progressView.backgroundColor = UIColor.clear
+    }
+}
 
+
+extension WeiBoPhotoBrowserViewCell {
+    
+    func setupContent(picURL: String?) {
+        guard let picURL = picURL,
+            let image = SDWebImageManager.shared().imageCache.imageFromDiskCache(forKey: picURL) else {
+                return
+        }
+        
+        // 计算imageView的frame
+        let x: CGFloat = 0
+        let width = UIScreen.main.bounds.width
+        let height = (width / image.size.width) * image.size.height
+        
+        var y: CGFloat = 0
+        if height > UIScreen.main.bounds.height {
+            y = 0
+        } else {
+            y = (UIScreen.main.bounds.height - height) * 0.5
+        }
+        imageView.frame = CGRect(x: x, y: y, width: width, height: height)
+        
+        // 设置图片
+        //imageView.image = image
+        progressView.isHidden = true
+        let largeURL = getLargePicURL(picURL: picURL)
+        imageView.sd_setImage(with: largeURL,
+                              placeholderImage: image,
+                              options: [],
+                              progress: { (current, total) in
+                                    self.progressView.progress = CGFloat(current) / CGFloat(total)
+                                }, completed: { (_, _, _, _) in
+                                    self.progressView.isHidden = true
+                            })
+        
+        scrollView.contentSize = CGSize(width: 0, height: height)
+    }
+    
+    
+    func getLargePicURL(picURL: String) -> URL {
+        let largeURL = picURL.replacingOccurrences(of: "/wap360/", with: "/large/")
+        return URL(string: largeURL)!
     }
 }
